14aa36a2669612ffde73a0e64e6de2d01354e9f8
The-Duchess <iarcher@pdx.edu>
2016-07-04 12:35:20 -0700

    clearing out draft for student table
    someone else is working on that and this was just me working to see if i was doing SQL correctly

--- schema/db_tables.sql
+++ schema/db_tables.sql
@@ -17,11 +17,3 @@ Editable   boolean     default true,
 term_id    bigserial PRIMARY KEY,
 Due_DATE   timestamp default NULL 
 );
-DROP TABLE IF EXISTS 'Student'
-CREATE TABLE 'Student' (
-Student_ID bigint PRIMARY KEY,
-Name_First varchar(20) NOT NULL,
-Name_Last  varchar(20) NOT NULL,
-email      varchar(30) NOT NULL,
-CAT_YEAR   varchar(20) NOT NULL
-);
