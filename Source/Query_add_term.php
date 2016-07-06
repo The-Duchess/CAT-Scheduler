d85d99c8ac982cb9588923ca57145a674696e446
Graham Drakeley <gt.drakeley@gmail.com>
2016-07-06 05:53:05 -0700

    Added scripts and queries for displaying students (US103)
    -Also modified Query_add_term.php and Query_retrieve_active_terms.php
        to use proper global variable access via $GLOBALS PHP variable.

--- Source/Query_add_term.php
+++ Source/Query_add_term.php
@@ -5,7 +5,7 @@ function add_term($name, $start, $end, $due) {
     //  Assumes we want the default values for visible and editable fields
     $query = 'INSERT into Terms (term_name, start_date, end_date, due_date) VALUES($1, $2, $3, $4)';
 
-    return pg_query_params($CONNECTION, $query, array($name, $start, $end, $due));
+    return pg_query_params($GLOBALS['CONNECTION'], $query, array($name, $start, $end, $due));
 }
 
 ?>
--- Source/Query_retrieve_active_terms.php
+++ Source/Query_retrieve_active_terms.php
@@ -1,13 +1,15 @@
 <?php
 
-//  Returns a results object of query if successful, FALSE otherwise
+//  Returns a results object containing the names and unique ids of
+//  visible and editable terms in the database if successful,
+//  otherwise FALSE
 function retrieve_active_terms() {
     //  We want to retrieve a list of all terms that are both visibile
     //  and editable
     $query = 'SELECT term_name, term_id FROM Terms ' .
              'WHERE visible IS true and editable is TRUE';
 
-    return pg_query($CONNECTION, $query);
+    return pg_query($GLOBALS['CONNECTION'], $query);
 }
 
 ?>
