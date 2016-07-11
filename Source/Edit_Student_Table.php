95292be3d2e81209a79cf012c81a287cbbd5a991
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-07-11 13:31:55 -0700

    merge of backend and frontend

--- Source/Edit_Student_Table.php
+++ Source/Edit_Student_Table.php
@@ -3,44 +3,51 @@
 	// update a student's info
 	// this provides a number of functions to alter a specific student's value other than their primary key
 	// and the selection is done by the primary key
+
+	// these functions operate in a generic way where you feed the id of the student
+	// that is going to have a value changed and a new value for that column
+	// parameters:
+	//		id: the student id
+	// 		new: the new value
+
 	function edit_student_email($id, $new) {
 		// this will edit the student's email
 
-		$query = 'UPDATE Student SET Student_Email=$new WHERE Student_id=$id';
+		$query = 'UPDATE Student SET Student_Email=$2 WHERE Student_id=$1';
 
-		return pg_query($GLOBALS['CONNECTION'], $query);
+		return pg_query_params($GLOBALS['CONNECTION'], $query, $array($id, $new));
 	}
 
 	function edit_student_firstname($id, $new) {
 		// this will edit the student's first name
 
-		$query = 'UPDATE Student SET Student_FirstName=$new WHERE Student_id=$id';
+		$query = 'UPDATE Student SET Student_FirstName=$2 WHERE Student_id=$1';
 
-		return pg_query($GLOBALS['CONNECTION'], $query);
+		return pg_query_params($GLOBALS['CONNECTION'], $query, $array($id, $new));
 	}
 
 	function edit_student_lastname($id, $new) {
 		// this will edit the student's last name
 
-		$query = 'UPDATE Student SET Student_LastName=$new WHERE Student_id=$id';
+		$query = 'UPDATE Student SET Student_LastName=$2 WHERE Student_id=$1';
 
-		return pg_query($GLOBALS['CONNECTION'], $query);
+		return pg_query_params($GLOBALS['CONNECTION'], $query, $array($id, $new));
 	}
 
 	function edit_student_nick($id, $new) {
 		// this will edit the student's cat nick
 
-		$query = 'UPDATE Student SET Cat_Nickname=$new WHERE Student_id=$id';
+		$query = 'UPDATE Student SET Cat_Nickname=$2 WHERE Student_id=$1';
 
-		return pg_query($GLOBALS['CONNECTION'], $query);
+		return pg_query_params($GLOBALS['CONNECTION'], $query, $array($id, $new));
 	}
 
 	function edit_student_visible($id, $new) {
 		// this will edit the student's state s visible
 
-		$query = 'UPDATE Student SET Visible=$new WHERE Student_id=$id';
+		$query = 'UPDATE Student SET Student_Visible=$2 WHERE Student_id=$1';
 
-		return pg_query($GLOBALS['CONNECTION'], $query);
+		return pg_query_params($GLOBALS['CONNECTION'], $query, $array($id, $new));
 	}
 
 ?>
---
+++ Source/Query/term.php
@@ -0,0 +1,243 @@
+<?php
+
+//  Retrieves a results object containing all terms regardless of visibility
+//  or editability, otherwise false
+//  PARAMETERS:
+//      kwargs: associative array of keyword arguments
+//          order_by:   what field(s) to order the results by, default term_id
+//          ascend:     if the results should be in ascending order, default false
+//          limit:      the max number of terms to retrieve, default none (null)
+function term_retrieve_all($kwargs=null) {
+    //  Default values
+    $order_by = "term_id";
+    $ascend = false;
+    $limit = null;
+
+    //  Read kwargs if necessary
+    if ($kwargs) {
+        if (isset($kwargs['order_by'])) {
+            $order_by = $kwargs['order_by'];
+        }
+        if (isset($kwargs['ascend'])) {
+            $ascend = $kwargs['ascend'];
+        }
+        if (isset($kwargs['limit'])) {
+            $limit = $kwargs['limit'];
+        }
+    }
+
+    //  Create the query and querys params without limit clause
+    $query = "SELECT * FROM Term ORDER BY $1 " . ($ascend ? "ASC" : "DESC");
+    $params = array($order_by);
+
+    //  Add limit clause and limit param if necessary
+    if ($limit) {
+        $query .= " LIMIT $2";
+        array_push($params, $limit);
+    }
+
+    //  Return the results.
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+
+}
+
+
+//  Retrieves a results object containing all terms regardless of editability,
+//  otherwise false
+//  PARAMETERS:
+//      kwargs: associative array of keyword arguments
+//          order_by:   what field(s) to order the results by, default term_id
+//          ascend:     if the results should be in ascending order, default false
+//          limit:      the max number of terms to retrieve, default none (null)
+function term_retrieve_visible_all($kwargs=null) {
+    //  Default values
+    $order_by = "term_id";
+    $ascend = false;
+    $limit = null;
+
+    //  Read kwargs if necessary
+    if ($kwargs) {
+        if (isset($kwargs['order_by'])) {
+            $order_by = $kwargs['order_by'];
+        }
+        if (isset($kwargs['ascend'])) {
+            $ascend = $kwargs['ascend'];
+        }
+        if (isset($kwargs['limit'])) {
+            $limit = $kwargs['limit'];
+        }
+    }
+
+    //  Create the query and querys params without limit clause
+    $query = "SELECT * FROM Term WHERE visible IS true ORDER BY $1 " . ($ascend ? "ASC" : "DESC");
+    $params = array($order_by);
+
+    //  Add limit clause and limit param if necessary
+    if ($limit) {
+        $query .= " LIMIT $2";
+        array_push($params, $limit);
+    }
+
+    //  Return the results.
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+
+}
+
+
+//  Retrieves a results object containing all terms that start before
+//  or at a certain date, otherwise FALSE
+//  PARAMETERS:
+//      kwargs: associative array of keyword arguments
+//          start:  the start date to compare to, default 1 year before now
+//          ascend: if the results should be in ascending order, default false
+//          limit:  the max number of terms to retrieve, default none (null)
+function term_retrieve_by_start($kwargs=null) {
+    //  Default values
+    $start = null;
+    $ascend = false;
+    $limit = null;
+
+    //  Read kwargs if necessary
+    if ($kwargs) {
+        if (isset($kwargs['start'])) {
+            $start = $kwargs['start'];
+        }
+        if (isset($kwargs['ascend'])) {
+            $ascend = $kwargs['ascend'];
+        }
+        if (isset($kwargs['limit'])) {
+            $limit = $kwargs['limit'];
+        }
+    }
+
+    //  Initialize start to default value if not passed
+    if (!$start) {
+        $start = (new DateTime("now"))->modify("-1 year")->format("Y-m-d");
+    }
+
+    //  Create the query and querys params without limit clause
+    $query = "SELECT * FROM Term WHERE start_date >= $1 ORDER BY start_date " . ($ascend ? "ASC" : "DESC");
+    $params = array($start);
+
+    //  Add limit clause and limit param if necessary
+    if ($limit) {
+        $query .= " LIMIT $2";
+        array_push($params, $limit);
+    }
+    
+    //  Return the results.
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+
+}
+
+
+//  Retrieves a results object containing all terms that start before
+//  or at a certain date and are visible, otherwise FALSE
+//  PARAMETERS:
+//      kwargs: associative array of keyword arguments
+//          start:  the start date to compare to, default 1 year before now
+//          ascend: if the results should be in ascending order, default false
+//          limit:  the max number of terms to retrieve, default none (null)
+function term_retrieve_visible_by_start($kwargs=null) {
+    //  Default values
+    $start = null;
+    $ascend = false;
+    $limit = null;
+
+    //  Read kwargs if necessary
+    if ($kwargs) {
+        if (isset($kwargs['start'])) {
+            $start = $kwargs['start'];
+        }
+        if (isset($kwargs['ascend'])) {
+            $ascend = $kwargs['ascend'];
+        }
+        if (isset($kwargs['limit'])) {
+            $limit = $kwargs['limit'];
+        }
+    }
+
+    //  Initialize start to default value if not passed
+    if (!$start) {
+        $start = (new DateTime("now"))->modify("-1 year")->format("Y-m-d");
+    }
+
+    //  Create the query and querys params without limit clause
+    $query = "SELECT * FROM Term WHERE visible IS true AND start_date >= $1 ORDER BY start_date " . ($ascend ? "ASC" : "DESC");
+    $params = array($start);
+
+    //  Add limit clause and limit param if necessary
+    if ($limit) {
+        $query .= " LIMIT $2";
+        array_push($params, $limit);
+    }
+    
+    //  Return the results.
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+
+}
+
+
+//  Updates a term in the database, returns FALSE on a failure
+//  PARAMETERS:
+//      id:     the id of the term to update
+//      fields: an associative array of db data fields and their
+//              updated values. only fields passed as keys in
+//              this array will be updated
+//      check:  whether to verify there are no invalid fields in
+//              the fields parameter, small hit to performance.
+//              default TRUE
+function term_update($id, $fields, $check=true) {
+    //  Check that all fields are valid if required
+    if ($check) {
+        $valid_fields = array("term_name", "start_date", "end_date", "due_date", "visible", "editable");
+        foreach ($fields as $field => $val) {
+            if (!in_array(strtolower($field), $valid_fields)) {
+                return false;
+            }
+        }
+    }
+
+    //  Generate the query and its parameters
+    $field_arr = array();
+    $params = array($id);
+    $counter = 2;
+    foreach ($fields as $field => $val) {
+        array_push($field_arr, $field . "=$" . $counter);
+        array_push($params, $val);
+        $counter++;
+    }
+    $query = "UPDATE Term SET " . implode(", ", $field_arr) . " WHERE term_id=$1";
+    echo $query . "\n";
+    foreach ($params as $param) {
+        echo $param . "\n";
+    }
+
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+}
+
+// --
+
+//  Returns result object of query if successful, FALSE otherwise
+function add_term($name, $start, $end, $due) {
+    //  Assumes we want the default values for visible and editable fields
+    $query = 'INSERT into Terms (term_name, start_date, end_date, due_date) VALUES($1, $2, $3, $4)';
+
+    return pg_query_params($GLOBALS['CONNECTION'], $query, array($name, $start, $end, $due));
+}
+
+// --
+
+//  Returns a results object containing the names and unique ids of
+//  visible and editable terms in the database if successful,
+//  otherwise FALSE
+function retrieve_active_terms($kwargs=null) {
+    //  We want to retrieve a list of all terms that are both visibile
+    //  and editable
+    $query = 'SELECT term_name, term_id FROM Term ' .
+             'WHERE visible IS true AND editable IS TRUE';
+
+    return pg_query($GLOBALS['CONNECTION'], $query);
+}
+
+?>
