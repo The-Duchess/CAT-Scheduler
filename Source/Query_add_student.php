111b943f25b01aa4472b1eb2843b0a2d7f75527a
codywyatt <cwyatt@pdx.edu>
2016-07-11 19:37:02 -0700

    Removed useless column calls
    removed firstname lastname email and catname

--- Source/Query_add_student.php
+++ Source/Query_add_student.php
@@ -3,15 +3,15 @@
 // add a student to the students table
 // set id, first name, last name and email
 // all other values can be default and are not required for entry
-function add_student($id, $firstname, $lastname, $email) {
+function add_student($id) {
 
 	// values assumed to be default
 	// - cat nick
 	// - join / leave date (leave may not be available)
 	// - visible should be defaulted to yes
-	$query = 'INSERT into Students (Student_id, Student_FirstName, Student_LastName, Student_Email) VALUES ($1, $2, $3, $4)';
+	$query = 'INSERT into Students (Student_id) VALUES ($1)';
 
-	return pg_query_params($GLOBALS['CONNECTION'], $query, array($id, $firstname, $lastname, $email));
+	return pg_query_params($GLOBALS['CONNECTION'], $query, array($id));
 }
 
 ?>
