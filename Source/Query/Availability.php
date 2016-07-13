ef3d61beeb9245c81d3f5ec59db30c0c6e65c8f2
codywyatt <cwyatt@pdx.edu>
2016-07-13 13:44:16 -0700

    US131: Added Ability to retrieve availability
    Added way to retrieve student availability for given term
    (Temporary setup to get availability, waiting on availability page)

--- Source/Query/Availability.php
+++ Source/Query/Availability.php
@@ -2,4 +2,25 @@
 
 // Availability
 
+//US131
+//Retrieves the availability for a select student on a select term
+// Is given both the student id and the term id
+      //--Needs changing once availability page is created
+function retrieve_availability_for_student($kwargs=null) {
+    if ($kwargs) {
+        if (isset($kwargs['student_id'])) {
+            $s_id = $kwargs['student_id'];
+        }
+        if (isset($kwargs['term_id'])) {
+            $t_id = $kwargs['term_id'];
+        }
+    }
+	$params = array($s_id, $t_id);
+    $query = "SELECT student_username, term_name, block_day, block_hour, block_preference FROM hour_block, student, term
+WHERE student.student_id = hour_block.student_id 
+AND term.term_id = hour_block.term_id 
+AND term.term_id = $2 AND student.student_id = $1";
+    return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
+}
+
 ?>
