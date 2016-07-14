6af088fe3733d16170403269efaa511b94cca7aa
codywyatt <cwyatt@pdx.edu>
2016-07-13 17:10:09 -0700

    Updated Test Availability to not use kwargs
    Updated Test Availability to have ids as passed in values and not kwargs
    Work still needed on kwargs but will show all availability for student_id with regards to term_id

--- Source/test/test_retrieve_avail.php
+++ Source/test/test_retrieve_avail.php
@@ -8,24 +8,29 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
-include "./retrieve_availability.php";
+include "../Query/Availability.php";
 //  Database connection
 if (!($CONNECTION = pg_connect("host=localhost port=5432 dbname=Cat user=guest password=Fido"))) {
     echo "<p>Connection Failed</p>\n";
     exit();
 }
-$kwargs = array(
-    "student_id" => (3),
-    "term_id" => (2));
 
-if (!($result = retrieve_availability_for_student($kwargs))) {
+$kwargs2 = array("block_hour" => (8));
+//$kwargs3 = array("block_day" => ('Monday');
+//$kwargs4 = array("block_day" => ('Monday'), "block_hour" => (8));
+
+$student_id = 4;
+$term_id = 2;
+
+if (!($result = retrieve_availability_for_student($student_id, $term_id, $kwargs2))) {
         return false;
     } else if (!($students = pg_fetch_all($result))) {
         return false;
     }
     foreach ($students as $student) {
-    echo "<p>" . $student['term_name']." ".$student['student_username'] ." ". $student['block_preference'] ." ". $student['block_day']." ".$student['block_hour']."</p>\n";
-    }
+			echo "<p>" . $student['term_name']." ".$student['student_username'] ." ". $student['block_preference'] ." ". $student['block_day']." ".$student['block_hour']."</p>\n";
+			}
+			
 ?>
 </body>
 </html>
