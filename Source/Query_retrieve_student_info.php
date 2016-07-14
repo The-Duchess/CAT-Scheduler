06f06d67bb9404cbbcd3d73c95158640f30da1b9
Jonathan Castle <castle2@ada.cs.pdx.edu>
2016-07-13 18:41:59 -0700

    US118: implemented query to retrieve all students in a selected term (student id and student username)

--- Source/Query_retrieve_student_info.php
+++ Source/Query_retrieve_student_info.php
@@ -1,11 +1,10 @@
 <?php
 
+// retrieves an array of students constrained by a specific term
+function retrieve_student_info($termid) {
 
-function retrieve_student_info($studentid) {
-
-
-	$query = "SELECT *,student_email FROM hour_block ".
-	"JOIN student ON hour_block.student_id=student.student_id WHERE hour_block.student_id='$studentid'";
+	// find all students who are in the specified term and return an array of their student ids and student usernames
+	$query = "SELECT DISTINCT student.student_id, student_username FROM hour_block INNER JOIN student ON hour_block.student_id=student.student_id WHERE hour_block.term_id='$termid'";
     return pg_query($GLOBALS['CONNECTION'], $query);
 }
 
