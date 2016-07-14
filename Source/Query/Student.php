967d9477b0cec1161f0b81ae3c58b952728b238b
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-07-14 14:39:18 -0700

    merging changes for Query_retreive_student_info.php into lib loc

--- Source/Query/Student.php
+++ Source/Query/Student.php
@@ -179,4 +179,12 @@ function add_student($id, $student_uname, $joind) {
         //  Return results object
         return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
     }
+
+    // retrieves an array of students constrained by a specific term
+	function retrieve_student_info($termid) {
+
+	    $query = "SELECT student_id, student_username FROM student WHERE student_id IN (SELECT student_id FROM hour_block WHERE term_id=$1)";
+
+		return pg_query_params($GLOBALS['CONNECTION'], $query, array($termid));
+	}
 ?>
