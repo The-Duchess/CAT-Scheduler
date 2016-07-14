e469bd124ef82bd3171313a571a0950f12fe1b5e
nimasns <nimasns@gmail.com>
2016-07-14 13:07:13 -0700

    Fixed US118: replaced INNEr Join with IN and returing a pg_query_param()

--- Source/Query_retrieve_student_info.php
+++ Source/Query_retrieve_student_info.php
@@ -3,9 +3,8 @@
 // retrieves an array of students constrained by a specific term
 function retrieve_student_info($termid) {
 
-	// find all students who are in the specified term and return an array of their student ids and student usernames
-	$query = "SELECT DISTINCT student.student_id, student_username FROM hour_block INNER JOIN student ON hour_block.student_id=student.student_id WHERE hour_block.term_id='$termid'";
-    return pg_query($GLOBALS['CONNECTION'], $query);
-}
+    $query = "SELECT student_id, student_username FROM student WHERE student_id IN (SELECT student_id FROM hour_block WHERE term_id=$1)";
 
+return pg_query_params($GLOBALS['CONNECTION'], $query, array($termid));
+}
 ?>
