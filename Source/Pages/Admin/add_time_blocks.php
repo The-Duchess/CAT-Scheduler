773e993c1a8177b4137c74baf002c3c1c3eaa4de
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-07-14 13:13:16 -0700

    testing

--- Source/Pages/Admin/add_time_blocks.php
+++ Source/Pages/Admin/add_time_blocks.php
@@ -5,29 +5,27 @@
 
 <body>
     <p>It begins</p>
+
+
 <?php
+
+
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 include "../Query/term.php";
 
 //  Database connection
+/*
 if (!($CONNECTION = pg_connect("host=capstonecatteam.hopto.org port=5432 dbname=Cat user=guest password=FIDO"))) {
     echo "<p>Connection Failed</p>\n";
     exit();
 }
-
-// need fields
-//      - start date , date time
-//      - end date   , date time
-//      - name       , varchar
-//      - due        , date time
+*/
 
 $s_time = "";
 $e_time = "";
 $name_v = "";
 $due_v  = "";
-
-
 
 echo "<form action=\"" . htmlentities($_SERVER['PHP_SELF']) . "\" method=\"post\">
 Name: <input type="text" name="name_v"><br>
@@ -35,16 +33,12 @@ Name: <input type="text" name="name_v"><br>
 </form>";
 
 
-
 $name_v = $_POST["name_v"];
 
-<p> Term Name: <?php echo $name_v ?>.</p>
-
-// fetch values
-// on submit
-// run query
-
+echo "$name_v";
 
 ?>
+
+
 </body>
 </html>
