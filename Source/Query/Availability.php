a018005db369407f3da2c7498051a3557470f3f3
codywyatt <cwyatt@pdx.edu>
2016-07-13 17:07:32 -0700

    Updated Availability to use kwargs correctly
    Updated Availability to use kwargs correctly, still need to fix commented code

--- Source/Query/Availability.php
+++ Source/Query/Availability.php
@@ -1,26 +1,40 @@
 <?php
-
-// Availability
-
-//US131
-//Retrieves the availability for a select student on a select term
-// Is given both the student id and the term id
-      //--Needs changing once availability page is created
-function retrieve_availability_for_student($kwargs=null) {
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+function retrieve_availability_for_student($student_id, $term_id, $kwargs=null) {
+	// Default values
+	if (!is_int($student_id) OR (!is_int($term_id))) {
+		return false;
+	}
+	$block_day = null;
+	$block_hour = null;
+	//  Read kwargs if necessary
     if ($kwargs) {
-        if (isset($kwargs['student_id'])) {
-            $s_id = $kwargs['student_id'];
+        if (isset($kwargs['block_day'])) {
+            $block_day = $kwargs['block_day'];
         }
-        if (isset($kwargs['term_id'])) {
-            $t_id = $kwargs['term_id'];
+        if (isset($kwargs['block_hour'])) {
+            $block_hour = $kwargs['block_hour'];
         }
     }
-	$params = array($s_id, $t_id);
-    $query = "SELECT student_username, term_name, block_day, block_hour, block_preference FROM hour_block, student, term
-WHERE student.student_id = hour_block.student_id 
-AND term.term_id = hour_block.term_id 
-AND term.term_id = $2 AND student.student_id = $1";
+	$params = array($student_id, $term_id);
+    $query = "SELECT student_username, term_name, block_day, block_hour, block_preference 
+	FROM hour_block, student, term 
+	WHERE student.student_id = hour_block.student_id 
+	AND term.term_id = hour_block.term_id 
+	AND student.student_id = $1
+	AND term.term_id = $2";
+	
+/*	
+	array_push($params, days $block_day);
+	array_push($params, hours $block_hour);
+if ($block_day){
+	$query .= " AND block_day = $3";
+}
+if ($block_hour){
+    $query .= " AND block_hour = $4";
+}
+*/
     return pg_query_params($GLOBALS['CONNECTION'], $query, $params);
 }
-
 ?>
