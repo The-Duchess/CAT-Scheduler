9dbb1942b4e51ebbe4ffe0321b6a5f9ad01bbc74
Graham Drakeley <gt.drakeley@gmail.com>
2016-07-14 18:40:41 -0700

    US141 - Improved comments

--- Source/Utility/utils.php
+++ Source/Utility/utils.php
@@ -10,15 +10,30 @@ function date_selector($submit_ident, $future=1) {
     static $id = 0;
     $id++;
 
-    //  Initialize local variables
+    /*  BLOCK: Initialize local variables
+     *  We generate unique identifiers for the forms using the id
+     *      variable.
+     *  We also need to initialize some arrays that will
+     *      be used to generate both the month and day dropdowns
+     *  */
     $year_ident = "selectYearDateSelector" . $id;
     $month_ident = "selectMonthDateSelector" . $id;
     $day_ident = "selectDayDateSelector" . $id;
     $months = array("January" => 1, "February" => 2, "March" => 3, "April" => 4, "May" => 5, "June" => 6,
         "July" => 7, "August" => 8, "September" => 9, "October" => 10, "November" => 11, "December" => 12);
     $months31 = array(1, 3, 5, 7, 8, 10, 12);
+    //  END BLOCK
 
-    //  Create year dropdown
+    /*  BLOCK: Create year dropdown
+     *  First we echo the html code for a select form.
+     *  Next we use the current date offset by the $future variable to
+     *      determine the year furthest in the future we will accept.
+     *      Our minimum year is the year PSU was founded.
+     *  Next we use a for loop to construct the year options for the
+     *      dropdown taking care to select the previously selected month
+     *      if an update occured after dropdown was already changed
+     *  Finally we end the select block
+     */
     echo "<select name=\"" . $year_ident . "\" onchange=\"this.form.submit()\">\n";
     $thisyear = intval((new DateTime("now"))->modify("+" . $future . " year")->format("Y"));
     for ($year=$thisyear; $year>=1946; $year--) {
@@ -29,8 +44,18 @@ function date_selector($submit_ident, $future=1) {
         }
     }
     echo "</select>\n";
+    //  END BLOCK
 
-    //  Create month dropdown
+    /*  BLOCK: Create month dropdown
+     *  First ewe echo the html code for a select form
+     *  Next we set the forms $_POST entry if it was not already set
+     *      so it can be used by the day dropdown to determine the
+     *      number of days to display
+     *  Next we use a for loop to construct the month options for the
+     *      dropdown taking care to select the previously selected month
+     *      if an update occured after the dropdown was changed
+     *  Finally we end the select block
+     */
     echo "<select name=\"" . $month_ident . "\" onchange=\"this.form.submit()\">\n";
     if (!isset($_POST[$month_ident])) {
         $_POST[$month_ident] = 1;
@@ -43,8 +68,18 @@ function date_selector($submit_ident, $future=1) {
         }
     }
     echo "</select>\n";
+    //  END BLOCK
 
-    //  Create day dropdown
+    /*  BLOCK: Create day dropdown
+     *  First we echo the html code for a select form
+     *  Next we determine the number of days necessary to display.
+     *      If the month was February we check for leap years otherwise
+     *      we see of the date was in $months31 for 31 days, 30 otherwise
+     *  Next for loops construct the day options for the dropdown taking
+     *      care to select the previously selected day if an update
+     *      occured after the dropdown was changed
+     *  Finally we end the select block
+     */
     echo "<select name=\"" . $day_ident . "\">\n";
     if ($_POST[$month_ident] == $months["February"]) {
         $intyear = intval($_POST[$year_ident]);
