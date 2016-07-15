1d858b0ac0cd0b786f0d43008d72b26f96b2f327
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-07-14 20:12:45 -0700

    merging changes from queries into lib loc

--- Source/Query/Availability.php
+++ Source/Query/Availability.php
@@ -155,4 +155,14 @@ function update_availability_blocks($input_term_id, $input_blocks, $kwargs=null)
 		pg_query($GLOBALS['CONNECTION'], "COMMIT");
 		return true;
 }
+
+// retrieves the shift preference for a specific student by a specific term
+function retrieve_shift_preference($studentid, $termid) {
+
+    //Selecet the shift preference from the shift preference table when
+    //student Id matches the entered Id and term Id matches the term ID
+    $query = "SELECT shift_preference FROM shift_preference WHERE term_id=$1 AND student_id=$2";
+
+    return pg_query_params($GLOBALS['CONNECTION'], $query, array($termid, $studentid));
+}
 ?>
--- Source/Query/term.php
+++ Source/Query/term.php
@@ -241,4 +241,38 @@ function add_term($name, $start, $end, $due) {
     return pg_query_params($GLOBALS['CONNECTION'], $query, array($name, $start->format("Y-m-d"), $end->format("Y-m-d"), $due->format("Y-m-d")));
 }
 
+// deactivate a student in the students table
+// set the Visible value to false
+// all other values can be default and are not required to change
+function deactivate_term($id) {
+
+    //Throw an error if student does not exist in the data base
+    //  $query = "SELECT * FROM term WHERE term_id = '$id'";
+    $query = "SELECT * FROM term WHERE term_id=$1";
+    //  $result = pg_query($GLOBALS['CONNECTION'], $query);
+    $result = pg_query_params($GLOBALS['CONNECTION'], $query, array($id));
+    if(pg_num_rows($result) == 0) {
+        echo "Term Does not exist<br>";
+        return false;
+    }
+    
+    //Throw an error if the student is already activated
+    //  $query = "SELECT * FROM term WHERE term_id = '$id' AND editable is FALSE";
+    $query = "SELECT * FROM term WHERE term_id=$1 AND editable is FALSE";
+    //  $result = pg_query($GLOBALS['CONNECTION'], $query);
+    $result = pg_query_params($GLOBALS['CONNECTION'], $query, array($id));
+    if (pg_num_rows($result) != 0) {
+        echo "term is already deactivated<br>";
+        return false;
+    }
+    //The only value is chanhing is the visibily since we are going to keep all
+    //of the student information
+    //  $query = "UPDATE term SET editable = false WHERE term_id = '$id'";
+    $query = "UPDATE term SET editable=false WHERE term_id=$1";
+
+    //  return pg_query($GLOBALS['CONNECTION'], $query);
+    return pg_query_params($GLOBALS['CONNECTION'], $query, array($id));
+}
+
+
 ?>
