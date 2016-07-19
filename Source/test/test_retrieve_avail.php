c81742701c5c263abb831635ca5be5ec25b826a1
codywyatt <cwyatt@pdx.edu>
2016-07-19 15:19:39 -0700

    Updated Test retrieve avail to have kwargs
    Fixed optional block_hour and block_day kwargs
    selecting one or both will select only those from the database
    Kept new connection for database

--- Source/test/test_retrieve_avail.php
+++ Source/test/test_retrieve_avail.php
@@ -1,14 +1,17 @@
 <html>
 <head>
-    <title>Test</title>
+    <title>Retreive Availability Test</title>
+	        <style type="text/css">
+            tab1 { padding-left: 4em; }
+            tab2 { padding-left: 8em; }
+        </style>
 </head>
 
 <body>
-    <p>It begins</p>
+    <p>Availability</p>
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
 include "../Query/Availability.php";
+
 //  Database connection
 if (!($CONNECTION = pg_connect("host=db.cecs.pdx.edu port=5432 dbname=simca user=simca password=hk8#9Yyced"))) {
 //if (!($CONNECTION = pg_connect("host=localhost port=5432 dbname=Cat user=guest password=Fido"))) {
@@ -16,21 +19,27 @@ if (!($CONNECTION = pg_connect("host=db.cecs.pdx.edu port=5432 dbname=simca user
     exit();
 }
 
+$kwargs = array();
 $kwargs2 = array("block_hour" => (8));
-//$kwargs3 = array("block_day" => ('Monday');
-//$kwargs4 = array("block_day" => ('Monday'), "block_hour" => (8));
+$kwargs3 = array("block_day" => ('Monday'));
+$kwargs4 = array("block_day" => ('Monday'), "block_hour" => (8));
 
-$student_id = 3;
+$student_id = 4;
 $term_id = 2;
 
-if (!($result = retrieve_availability_for_student($student_id, $term_id))) {
+if (!($result = retrieve_availability_for_student($student_id, $term_id, $kwargs))) {
         return false;
     } else if (!($students = pg_fetch_all($result))) {
         return false;
     }
+
+		echo "<p>Term: " . $students[0]['term_name'] ."</p>\n";
+		echo "<p><tab1>Student: ".$students[0]['student_username'] ."<tab1></p>\n";
+	
+	
     foreach ($students as $student) {
-			echo "<p>" . $student['term_name']." ".$student['student_username'] ." ". $student['block_preference'] ." ". $student['block_day']." ".$student['block_hour']."</p>\n";
-			}
+		echo "<p><tab2>". $student['block_day']." ".$student['block_hour']. " " .$student['block_preference'] ."<tab2></p>\n";
+		}
 			
 ?>
 </body>
