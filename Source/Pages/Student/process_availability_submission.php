1b6fb0ff3efa9d3f59307c46c8f62a9af4ab632d
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-07-20 18:50:48 -0700

    test

--- Source/Pages/Student/process_availability_submission.php
+++ Source/Pages/Student/process_availability_submission.php
@@ -3,36 +3,6 @@
 include "../../Query/Student.php"
 include "../../Query/Availability.php"
 
-//function test($selected_term){
-
-// POST KEY VALUES
-// "term_name" => term name
-// "term_id"   => term id
-// the availabilities are passed in the form
-// day and hour are concatonated together as the key to the value
-// 			Monday9 => A
-// 	to show monday at 9 is A
-
-/*
-
-availabilities = { day => "DAY NAME", hours => [hours], shift => "pref" }
-
-POST.each do |p|
-	if p == "term_name" or p == "term_id" or p == "shift_preference"
-		next
-	else
-		hour_v.push p[-1]
-		day = p[0..-2]
-		availabilities.push { :day => day, ..}
-	end
-end
-
-student_uname = $_SERVER['PHP_AUTH_US']
-student_id = get_student_id_by_username(student_uname)
-*/
-
-//}
-
 function submit_availabilities(){
 	
 	$student_uname = $_SERVER['PHP_AUTH_US'];
@@ -43,14 +13,6 @@ function submit_availabilities(){
 
 	// things needed for time submission
 	// insert_availability_block($input_term_id, $input_day, $input_hour, $input_pref, $kwargs=null)
-	// $_POST contains
-	// value { id => 'dayhour', value => 'availability' }
-	// so we want to create
-	// input_term_id = $_POST['term_id']
-	// input_day = $_Post['id'][0..-1]
-	// input_hour = $_POST['id'][-1]
-	// input_pref = $_POST['value']
-	// create kwargs { student_id => $student_id }
 
 	foreach ($_POST as $key => $val) {
 		if ($key == "term_name" || $key == "term_id" || $key == "shift_preference") {
@@ -86,7 +48,7 @@ function submit_availabilities(){
 
 		}
 
-		update_availability_block($input_term_id, $input_bocks, array("student_id" => $student_id));
+		update_availability_blocks($input_term_id, $input_bocks, array("student_id" => $student_id));
 	}
 
 }
