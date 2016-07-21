7964f9e1b2727a5d404e22f189d43952af8061be
Alexander Simchuk <alsim114@gmail.com>
2016-07-20 18:17:25 -0700

    US138: Create login page
    NEW FILE: Source/Pages/login_handler.php
    NEW FILE: Source/Pages/login_page.php

---
+++ Source/Pages/login_handler.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('../Query/Student.php');
+
+//connect to database
+if (!($CONNECTION = pg_connect("host=db.cecs.pdx.edu port=5432 dbname=simca user=simca password=hk8#9Yyced"))) {
+    echo "<p>Connection Failed</p>\n";
+    exit();
+}
+
+//check if user is in the database, if not insert them
+if ($result = get_student_id_by_username($_SERVER['PHP_AUTH_USER']) && pg_num_rows($result) == 0) {
+    $new_user = $_SERVER['PHP_AUTH_USER'];
+    $join_date = new Datetime("now");
+    if (!add_student($new_user, $join_date)) {
+        echo "ERROR: Logged in student not in database and insertion failed";
+    }
+} else {
+    echo "Could not determine if user is in database or not";
+}
+
+//check if the user is an admin or not
+$admin = false;
+//TODO: when LDAP is intigrated, change logic to set $admin flag
+if ($_SERVER['PHP_AUTH_USER'] == bowzr) {
+    $admin = true;
+}
+
+?>
