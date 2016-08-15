581b34c8baebaa5406f66849095c94359bd2ea36
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-08-15 16:36:02 -0700

    fix

--- Source/Pages/Admin/show-students.php
+++ Source/Pages/Admin/show-students.php
@@ -3,10 +3,10 @@ function show_students($student_res) {
 ?>
 <?php foreach ($student_res as $student_uname) { ?>
           <?php
-               echo "<input type=\"radio\" checked=\"checked\" name=" . $student_uname . "value=\"TRUE\"" . "<?= (1 == 1 ? ' checked ' : ' checked ') ?>>";
+               echo "<input type=\"radio\" checked=\"checked\" name=$student_uname value=\"TRUE\"" . "<?= (1 == 1 ? ' checked ' : ' checked ') ?>>";
           ?>
           <?php
-               echo "<label for=" . $student_uname . ">" . $student_uname . "</label>";
+               echo "<label for=$student_uname>$student_uname</label>";
           ?>
           <br>
 <?php } ?>
