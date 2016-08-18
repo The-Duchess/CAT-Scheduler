032e5bba4f17302171ae07fe8a2716935b0c9301
Alice "Duchess" Archer <iarcher@pdx.edu>
2016-08-17 20:37:35 -0700

    fix

--- Source/Pages/Admin/email_reminder.php
+++ Source/Pages/Admin/email_reminder.php
@@ -68,17 +68,15 @@ if (!($CONNECTION = fido_db_connect())) {
      to the email
 -->
      <form action="" method="post">
-          <div style="height:300px;
-          width:300px;border1px solid
-          #00FF00; overflow:auto">
-          <?php
+
+          <div class="pre-scrollable">
           foreach ($student_res as $student_uname) {
                          echo "<input type=\"checkbox\" checked=\"checked\" name=\"students[]\" value=$student_uname />";
                          echo "<label for=$student_uname>&nbsp; $student_uname </label>";
                          echo "<br>";
           }
           ?>
-          </div>
+          </div> <!--  end scrollbox  -->
                     <br>
      <label>Subject:</label>
      <input type="text" name="subject" size="80"><br>
